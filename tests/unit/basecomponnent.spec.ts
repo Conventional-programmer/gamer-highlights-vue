bcf4208b5e546eec41c6c68ca42ddd08bc901649
Cas <407529@student.fontys.nl>
2021-04-19 14:30:47 +0200

    fixed test by using mount instead of shallow mount

--- tests/unit/basecomponnent.spec.ts
+++ tests/unit/basecomponnent.spec.ts
@@ -1,12 +1,12 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import BaseProfile from '@/components/BaseProfile.vue'
 import User from "@/class/user";
 
 const user = { username: 'gertje', description:'gertje is een mooie meid',imageUrl:'https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png'} as User
-const wrapper = shallowMount(BaseProfile, {
+const wrapper = mount(BaseProfile, {
   props: { user: user}
 })
-describe('HelloWorld.vue', () => {
+describe('baseprofile.vue', () => {
   it('view username', () => {
     wrapper.text()
     expect(wrapper.text()).toContain('gertje')
