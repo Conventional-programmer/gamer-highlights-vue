0acbd0440bdd1ff00c3022c91cd3720e3a9d80fd
Cas <407529@student.fontys.nl>
2021-10-13 22:39:54 +0200

    logout when token expired

---
+++ src/service/authentication/auth-service.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost/auth/';
+
+class AuthService {
+    login(user) {
+        return axios
+            .post(API_URL + 'login', {
+                username: user.username,
+                password: user.password
+            })
+            .then(response => {
+                if (response.data.accessToken) {
+                    localStorage.setItem('user', JSON.stringify(response.data));
+                }
+                console.log(response);
+                return response.data;
+            });
+    }
+
+    logout() {
+        console.log('logout aut-service')
+        localStorage.removeItem('user');
+    }
+
+    register(user,recaptchaToken: string) {
+        return axios.post(API_URL + 'register', {
+            username: user.username,
+            email: user.email,
+            password: user.password,
+            token: recaptchaToken
+        });
+    }
+}
+
+export default new AuthService();
